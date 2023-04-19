377569611ba9c79e80c7a0c133d5fedd04e4d368
Eduard <edpufa@inspalamos.cat>
2023-04-19 19:10:29 +0200

    creant el projecte amb java

---
+++ ConectarMYSQL/src/main/java/ConnexioMySQL.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnexioMySQL
+{
+    public static void main(String[] args)
+    {
+        String url = "jdbc:mysql://192.168.2.138/koobing_app";
+        String usuario = "guillem";
+        String contrasena = "";
+
+
+        try {
+            Connection conexion = DriverManager.getConnection(url, usuario, contrasena);
+            System.out.println("Conexión establecida con éxito");
+
+            // Aquí puedes realizar tus operaciones de base de datos
+
+            conexion.close();
+            System.out.println("Conexión cerrada");
+        } catch (SQLException ex)
+        {
+            System.out.println("Error al conectar a la base de datos: " + ex.getMessage());
+        }
+    }
+}
